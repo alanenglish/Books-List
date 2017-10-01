f0f9c72d2a0809060dbafbb2bf5b73b9cf90a05c
Alan English <aenglish@manorhillcapital.com>
2017-10-01 18:09:10 -0500

    add condition into bookdetail component to show select a book text when activeBook has not been selected yet, add book detail to app

--- src/containers/BookDetail.js
+++ src/containers/BookDetail.js
@@ -3,8 +3,16 @@ import { connect } from 'react-redux';
 
 class BookDetail extends Component {
   render() {
+    if (!this.props.book) {
+      return <div>Select a book to get started.</div>;
+    }
+
     return (
-      <div>Book Detail!</div>
+      <div>
+        <h3>Details For:</h3>
+        <div>{this.props.book.title}</div>
+        <div>{this.props.book.pages}</div>
+      </div>
     );
   }
 }
